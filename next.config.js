27bf7e081cb7d540190f898904eed9e449ef660c
criativemarket-spec <criativemarket@gmail.com>
2026-05-23 08:25:06 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,6 +12,11 @@ const nextConfig = {
         hostname: 'i.ibb.co',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
     ],
   },
   transpilePackages: ['swiper'],
